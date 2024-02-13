62d6e151bad9894a5ff1fc7c69e1a9f5da507095
ANIL <134312212+XPECTO-7@users.noreply.github.com>
2024-02-13 07:05:19 +0530

    first commit

---
+++ lib/Pages/home_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HomePafge extends StatefulWidget {
+  const HomePafge({super.key});
+
+  @override
+  State<HomePafge> createState() => _HomePafgeState();
+}
+
+class _HomePafgeState extends State<HomePafge> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
